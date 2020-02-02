03756c2e66887f24550f89ee77832355272edc76
Betty Guo <bettyg3113@gmail.com>
2020-02-01 21:31:52 -0500

    added build

---
+++ build/precache-manifest.cbc63203f15fa34783bb6dea6e198187.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1de94139b823ab25127dcb5ae5689def",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d6754f2a6ef707071c3f",
+    "url": "/static/css/main.4ad5f89b.chunk.css"
+  },
+  {
+    "revision": "cc892edd47589dfaf628",
+    "url": "/static/js/2.41dc3276.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.41dc3276.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6754f2a6ef707071c3f",
+    "url": "/static/js/main.62a68cfe.chunk.js"
+  },
+  {
+    "revision": "744bfcfbecec0e7e666d",
+    "url": "/static/js/runtime-main.50db6f5c.js"
+  }
+]);
